9a60b875260ed419ad5676aed54ec27b526e5287
DuhBenhur <eduardo.benhur@gmail.com>
2021-12-07 18:58:24 -0300

    Criação do site

---
+++ R/lll.R
@@ -0,0 +1,30 @@
+setwd("C:/Users/Gauge/Documents/")
+#Criar projeto do portfolio
+usethis::create_package("portfolioEduardoBenHur")
+
+#Definir usuário do github
+usethis::use_git_config(user.name = "DuhBenhur", user.email = "eduardo.benhur@gmail.com")
+
+#Criar token para conexão com o github
+usethis::create_github_token()
+
+#Inserir ou trocar o token do Github (É necessário ter o git instalado)
+gitcreds::gitcreds_set()
+
+#Chamar o git para utilização (vai aparecer a aba ao na região do Environment)
+usethis::use_git()
+
+#Criar repositório no github
+usethis::use_github()
+
+#Transformar o repositório do github em uma page
+usethis::use_github_pages("master", "/docs")
+
+
+#Criação de um arquivo oculto doidão que preciso pesquisar pra que serve
+#list.files(all.files = T) Para verificar se o arquivo foi criado
+file.create(".nojekyll")
+#list.files(all.files = T) Para verificar se o arquivo foi criado
+
+#Criar o Blog
+distill::create_blog(".", "Portfolio Eduardo Ben-Hur", gh_pages = TRUE)
